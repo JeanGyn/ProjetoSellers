9502f0603f55c4b73efba88f389d059e62ed0a52
Jeanderson Alves <jeanderson.analista@gmail.com>
2020-11-06 09:13:33 -0300

    Inicio do projeto

---
+++ ProjetoSellers/Data/ProjetoSellersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSellers.Models;
+
+namespace ProjetoSellers.Data
+{
+    public class ProjetoSellersContext : DbContext
+    {
+        public ProjetoSellersContext (DbContextOptions<ProjetoSellersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
---
+++ ProjetoSellers/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ProjetoSellers.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
