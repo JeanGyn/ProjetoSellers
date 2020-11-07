ba621711d3c99f9be10b9ada0792321de6ea5df0
Jeanderson Alves <jeanderson.analista@gmail.com>
2020-11-07 09:28:24 -0300

    Criando Formulario

---
+++ ProjetoSellers/Services/SellerService.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoSellers.Data;
+using ProjetoSellers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoSellers.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoSellersContext _context;
+
+        public SellerService(ProjetoSellersContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
