7fa318b3e1b9b241cd7a98bbb8a4d66495dfb6a5
Jeanderson Alves <jeanderson.analista@gmail.com>
2020-11-10 06:13:02 -0300

    Implementacao das buscas

---
+++ ProjetoSellers/Services/Exception/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjetoSellers.Services.Exception
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
